daa12dae9aaa259de9a9677a87c62e78620370dd
Trevor Osterman <trevormosterman@gmail.com>
2025-10-08 10:07:23 -0700

    add intro name heading

--- src/ui/Sections/Intro/Intro.tsx
+++ src/ui/Sections/Intro/Intro.tsx
@@ -7,32 +7,35 @@ import "./Intro.styles.css";
 const Intro: React.FC = () => {
   return (
     <section className="intro">
-      <div className="intro__avatar">
-        <img
-          src={avatar}
-          alt="pixel animation"
-          style={{
-            imageRendering: "pixelated",
-            display: "block",
-          }}
-        />
-      </div>
+      <h1 className="intro__heading">Trevor Osterman</h1>
+      <div className="intro__content">
+        <div className="intro__avatar">
+          <img
+            src={avatar}
+            alt="pixel animation"
+            style={{
+              imageRendering: "pixelated",
+              display: "block",
+            }}
+          />
+        </div>
 
-      <div className="intro__ps">
-        <p>
-          Hello and welcome! My name is Trevor Osterman, and I appreciate you
-          taking the time to view my portfolio.
-        </p>
-        <p>
-          I am a full stack web developer with experience in loads of fun and
-          useful tools and technologies. My journey with engineering so far has
-          been a fun and rewarding experience, and I'm always looking for new
-          opportunities to learn and grow.
-        </p>
-        <p>
-          When you're ready, scroll down and I'll tell you a bit more about
-          myself.
-        </p>
+        <div className="intro__ps">
+          <p>
+            Hello and welcome! My name is Trevor Osterman, and I appreciate you
+            taking the time to view my portfolio.
+          </p>
+          <p>
+            I am a full stack web developer with experience in loads of fun and
+            useful tools and technologies. My journey with engineering so far has
+            been a fun and rewarding experience, and I'm always looking for new
+            opportunities to learn and grow.
+          </p>
+          <p>
+            When you're ready, scroll down and I'll tell you a bit more about
+            myself.
+          </p>
+        </div>
       </div>
     </section>
   );
